b5ed34f8c0ec30d85cb3ce74ac0fc88b8659b303
Michael Greenly <mgreenly@gmail.com>
2016-02-10 16:56:44 -0600

    finish aeson sample

--- hello-aeson/app/Main.hs
+++ hello-aeson/app/Main.hs
@@ -2,54 +2,69 @@
 
 module Main where
 
--- import qualified Data.Text.Encoding as T
--- import qualified Data.ByteString.Lazy as B
--- import Data.Aeson
--- 
--- 
--- main = do
---     let x = (encode (1 :: Integer) :: T.Text)
---     T.putStrLn $ T.decodeUtf8 $ B.toStrict $ encode ([1,2,3] :: [Int])
-
--- import qualified Data.Text.Lazy.IO as T
--- import qualified Data.Text.Lazy.Encoding as T
--- import qualified Data.Aeson as A
--- 
--- 
--- main = do
---     T.putStrLn $ T.decodeUtf8 $ A.encode (1 :: Integer)
---
-
 import           Control.Monad
 import           Data.Aeson
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Text.Show.ByteString       as B
 import           Data.Text.Encoding
 
-import qualified Data.Text as T
+import qualified Data.Text    as T
 import qualified Data.Text.IO as T
+
+data Pizza = Cheese | Sausage deriving Show
+
+data PizzaOrder = PizzaOrder
+    { quantity :: !Integer
+    , pizza    :: Maybe Pizza
+    } deriving Show
 
 data Person = Person
     { firstName  :: !T.Text
     , lastName   :: !T.Text
     , age        :: Integer
-    , likesPizza :: Bool
+    , pizzaOrder :: PizzaOrder
     } deriving Show 
 
-instance FromJSON Person where
+
+instance ToJSON Pizza where
+    toJSON _ = "a pizza of some kind"
+
+instance ToJSON PizzaOrder where
+    toJSON (PizzaOrder quantity pizza) =
+        object [ "quantity" .= quantity
+               , "pizza"    .= pizza
+               ]
+
+instance FromJSON PizzaOrder where
     parseJSON (Object v) =
-        Person <$> v .: "firstName"
-               <*> v .: "lastName"
-               <*> v .: "age"
-               <*> v .: "likesPizza"
+        PizzaOrder <$> v .: "quantity"
+                   <*> liftM parsePizza (v .: "pizza")
     parseJSON _ = mzero
 
+instance FromJSON Person where
+    parseJSON (Object o) =
+        Person <$> o .: "firstName"
+               <*> o .: "lastName"
+               <*> o .: "age"
+               <*> o .: "pizzaOrder" 
+    parseJSON _ = mzero
+
+instance ToJSON Person where
+ toJSON (Person firstName lastName age pizzaOrder) =
+    object [ "firstName"  .= firstName
+           , "lastName"   .= lastName
+           , "age"        .= age
+           , "pizzaOrder" .= pizzaOrder
+           ]    
+
+parsePizza :: String -> Maybe Pizza
+parsePizza "cheese"  = Just Cheese
+parsePizza "sausage" = Just Sausage
+parsePizza _         = Nothing 
+
 getJSON :: IO B.ByteString
-getJSON = B.readFile "sample.json" 
+getJSON = B.readFile "sample2.json" 
 
 main = do
-    d <- (decode <$> getJSON) :: IO (Maybe [Person])
-    case d of
-        Nothing -> Prelude.putStrLn "bang!"
-        Just (p:ps)  -> T.putStrLn $ lastName p
-
--- http://blog.raynes.me/blog/2012/11/27/easy-json-parsing-in-haskell-with-aeson/
+    decodedPerson <- (decode <$> getJSON) :: IO (Maybe Person)
+    B.putStrLn $ encode decodedPerson

3395dd0ebeef1d712e866d08374c925ff570e6f2
Michael Greenly <mgreenly@gmail.com>
2016-02-11 17:21:22 -0600

    complete option parsing sample

--- hello-option-parsing/README.md
+++ hello-option-parsing/README.md
@@ -1,3 +1,7 @@
 
   * [thoughtbot tutorial](https://robots.thoughtbot.com/applicative-options-parsing-in-haskell)
   * [24 days of hackage tutorial](https://ocharles.org.uk/blog/posts/2012-12-17-24-days-of-hackage-optparse-applicative.html)
+
+
+
+
--- hello-option-parsing/app/Main.hs
+++ hello-option-parsing/app/Main.hs
@@ -2,27 +2,55 @@ module Main where
 
 import Options.Applicative
 
-data Options = Options
-  { name     :: String
-  , pizza    :: String 
-  , quantity :: Integer 
-  , quiet    :: Bool 
-  }
+type Quantity = Integer
 
-sample :: Parser Options
-sample = Options
-  <$> argument str (metavar "NAME")
-  <*> strOption (long "pizza" <> metavar "PIZZA" <> value "cheese" <> help "Target for the greeting")
-  <*> option auto (long "quantity" <> value 1 <> metavar "QUANTITY" <> help "number of pizza") 
-  <*> switch (long "quiet" <> help "Whether to be quiet")
+type Pizza = String
+type Toppings = String
+type Size = Integer
+
+type Burger = String
+type Pickles = Bool
+
+data Command
+    = PizzaCommand Pizza Size Toppings
+    | BurgerCommand Burger Pickles
+
+data Options = Options
+    { quantity   :: Quantity
+    , subCommand :: Command
+    }
+
+parsePizza :: Parser Command
+parsePizza = PizzaCommand <$> argument str (metavar "PIZZA")
+                          <*> option auto (long "size" <> value 12 <> metavar "SIZE" <> help "size of pizza") 
+                          <*> strOption (long "toppings" <> metavar "TOPPINGS" <> help "toppings for pizza")
+
+
+parseBurger :: Parser Command
+parseBurger = BurgerCommand <$> argument str (metavar "BURGER")
+                            <*> switch (long "pickles" <> help "include pickles")
+
+parseCommand :: Parser Command
+parseCommand = subparser $
+    command "pizza" (parsePizza `withInfo` "order a pizza") <>
+    command "burger" (parseBurger `withInfo` "order a burger")
+
+parseOptions :: Parser Options
+parseOptions = Options <$> option auto (long "quantity" <> value 1 <> metavar "QUANTITY" <> help "quantity of item ordered") 
+                       <*> parseCommand 
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
 
 run :: Options -> IO ()
-run (Options name pizza quantity False) = putStrLn $ name ++ " likes " ++ pizza ++ " pizza x " ++ (show quantity)
+run (Options quantity (PizzaCommand pizza size toppings)) = putStrLn $ "pizza " ++ pizza ++ " " ++ show size ++ " toppings: " ++ toppings ++ " quantity: " ++ show quantity
+run (Options quantity (BurgerCommand burger pickles)) = putStrLn $ "burger " ++ burger ++ " " ++ show pickles ++ " quantity: " ++ show quantity
 run _ = return ()
 
 
 main :: IO ()
 main = execParser opts >>= run
   where
-   opts = info (helper <*> sample)
-               (progDesc "Print a greeting for NAME")
+   opts = info (helper <*> parseOptions)
+               (progDesc "This-is-the-program-description")

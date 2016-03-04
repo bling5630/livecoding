56d4666567647bd423f5935144aee2e98def7700
Michael Greenly <mgreenly@gmail.com>
2016-03-03 22:10:59 -0600

    add run-foo

---
+++ run-foo/app/Main.hs
@@ -0,0 +1,31 @@
+module Main where
+
+--
+-- constructor using tuple 
+--
+data Foo a b = Foo (a , b) deriving Show
+
+--
+-- create a function to extract the tuple from a Foo
+--
+runFoo :: Foo a b -> (a, b)
+runFoo (Foo (a, b)) = (a, b)
+
+--
+-- use record syntax to do the same all in one shot 
+--
+data Bar a b = Bar { runBar :: (a, b) } deriving Show
+
+main = do
+    print $ someFoo
+    print $ runFoo someFoo
+    print $ someBar1
+    print $ runBar someBar1
+    print $ someBar2 
+    print $ runBar someBar2
+    return ()
+  where someFoo = Foo(1, "bingo")
+        -- you can create a bar with record syntax
+        -- or you can create it with tuple syntax
+        someBar1 = Bar{runBar=(1,"bingo")} 
+        someBar2 = Bar(1, "bingo")
---
+++ run-foo/src/RunFoo.hs
@@ -0,0 +1,6 @@
+module RunFoo
+    ( foo
+    ) where
+
+foo :: String
+foo = "foo"

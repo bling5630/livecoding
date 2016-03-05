8f67708f2e3069ccbd294f3da091cf7c50921883
Michael Greenly <mgreenly@gmail.com>
2016-03-04 20:47:06 -0600

    look at data type definitions

--- run-foo/app/Main.hs
+++ run-foo/app/Main.hs
@@ -16,6 +16,11 @@ runFoo (Foo (a, b)) = (a, b)
 --
 data Bar a b = Bar { runBar :: (a, b) } deriving Show
 
+--
+--
+--
+data Point a b = Point { pointX :: a, pointY :: b } deriving Show
+
 main = do
     print $ someFoo
     print $ runFoo someFoo
@@ -23,9 +28,16 @@ main = do
     print $ runBar someBar1
     print $ someBar2 
     print $ runBar someBar2
+    print $ somePoint1
+    print $ somePoint2
     return ()
   where someFoo = Foo(1, "bingo")
         -- you can create a bar with record syntax
         -- or you can create it with tuple syntax
         someBar1 = Bar{runBar=(1,"bingo")} 
         someBar2 = Bar(1, "bingo")
+        -- this is true of all records.  records
+        -- just create accessor to functions but
+        -- still define the datatype the same
+        somePoint1 = Point{pointX=17, pointY=19} :: Point Int Int
+        somePoint2 = Point 17 19 :: Point Int Int

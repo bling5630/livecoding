eeeb785ba3b5628af8fc8dcd8f0f38b2cab524fb
Michael Greenly <mgreenly@gmail.com>
2016-03-29 16:39:45 -0500

    add hello-monad-transformer

--- hello-monad-transformer/app/Main.hs
+++ hello-monad-transformer/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad.Reader
+import Control.Monad.State
 
 import UI.NCurses
     ( Event(..)
@@ -20,68 +21,53 @@ import UI.NCurses
     )
 
 
+type AppConfig = Window
+
 type AppState  = Int
-type AppConfig = Int
+
+
+type App = ReaderT AppConfig (StateT AppState Curses)
+
 
 defState :: AppState
 defState = 42
-
-defConfig :: AppConfig
-defConfig = 5
 
 
 main = runCurses $ do
     setEcho False
     window <- defaultWindow
-    runReaderT (renderScreen defState) window
-    runReaderT (mainLoop defState) window
+    runStateT (runReaderT (renderScreen) window) defState
+    runStateT (runReaderT (mainLoop) window) defState
+
+liftCurses = lift . lift
 
 
-mainLoop :: AppState -> ReaderT Window Curses ()
-mainLoop curState = do
+mainLoop :: App ()
+mainLoop = do
     window <- ask
-    event <- lift $ getEvent window Nothing
+    event <- liftCurses $ getEvent window Nothing
     case event of
-        Nothing                   -> mainLoop curState
+        Nothing                   -> mainLoop
         Just (EventCharacter 'q') -> return ()
-        Just event'               -> let nxtState = updateState event' curState in
-                                       do renderScreen nxtState
-                                          mainLoop nxtState
+        Just event'               -> do 
+                                        modify (updateState event')
+                                        renderScreen
+                                        mainLoop
 
 
-renderScreen :: AppState -> ReaderT Window Curses ()
-renderScreen state = do
+renderScreen :: App ()
+renderScreen = do
+    state <- get
     window <- ask
-    lift $ updateWindow window $ do
+    liftCurses $ updateWindow window $ do
         moveCursor 0 0
         clearLine
         drawString $ show state
-    lift $ render
+    liftCurses $ render
 
 
 updateState :: Event -> AppState -> AppState
 updateState (EventCharacter 'h') curState = curState - 1
 updateState (EventCharacter 'l') curState = curState + 1 
 updateState _ curState = curState
-
-
-
-
---mainLoop :: Window -> AppState -> Curses ()
---mainLoop window curState = do
---    event <- getEvent window Nothing
---    case event of
---        Nothing                   -> mainLoop window curState
---        Just (EventCharacter 'q') -> return ()
---        Just event'               -> let nxtState = updateState event' curState in
---                                       do renderScreen window nxtState
---                                          mainLoop window nxtState
-
--- renderScreen :: Window -> AppState -> Curses ()
--- renderScreen window state = do
---     updateWindow window $ do
---         moveCursor 0 0
---         clearLine
---         drawString $ show state
---     render
 

f64366b27817195eaf40871251ad1d094e04b91c
Michael Greenly <mgreenly@gmail.com>
2016-04-16 14:36:04 -0500

    more samples in arrays

--- hello-array/app/Main.hs
+++ hello-array/app/Main.hs
@@ -4,17 +4,17 @@ import Data.List
 import Data.List.Split
 import Data.Array
 
--- 
--- 
---        . . . . . . . . .  
+--
+--
+--        . . . . . . . . .
 --        . . . . . . . . .
 --        . . * * * * . . .
 --        . . * * * * . . .
 --        . . * * * * . . .
 --        . . . . . . . . .
 --        . . . . . . . . .
--- 
--- 
+--
+--
 
 mapRows = 10
 mapCols = 10
@@ -22,7 +22,7 @@ mapLowerBounds = (0, 0)
 mapUpperBounds = (mapRows - 1, mapCols - 1)
 mapBounds = (mapLowerBounds, mapUpperBounds)
 
-defaultGameMap = listArray mapBounds 
+defaultGameMap = listArray mapBounds
     [ "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"
     , "#", "1", ".", ".", ".", ".", ".", ".", ".", "# "
     , "#", ".", "2", ".", ".", ".", ".", ".", ".", "# "
@@ -36,8 +36,24 @@ defaultGameMap = listArray mapBounds
     , "#", "#", "#", "#", "#", "#", "#", "#", "#", "# " ]
 
 
+arrayFst a = a ! (fst(bounds a))
+arrayLst a = a ! (snd(bounds a))
+
+subArray bounds = ixmap (bounds) id
+
 main = do
-    let subMap = ixmap ((2,2),(5,5)) id defaultGameMap 
-    let foo = splitEvery 4 $ concat $ elems subMap 
+    print $ myArray ! 1
+    print $ myArray ! (snd(bounds myArray))
+    print $ arrayFst myArray
+    print $ arrayLst myArray
+    print $ fmap (const 999) myArray
+    print $ mySubArray
+    print $ fmap (const 42) $ subArray (5,15) myArray
+    print $ myArray // [(1, 999)]
+    putStrLn "--------------"
     mapM_ print foo
+ where myArray = listArray (1,20) [x | x <- [1..20]]
+       mySubArray = ixmap (5,15) id myArray
+       subMap = ixmap ((2,2),(5,5)) id defaultGameMap
+       foo = chunksOf 4 $ concat $ elems subMap
 

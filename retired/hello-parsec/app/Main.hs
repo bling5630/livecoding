bf0e50f6dc80337d3fbd67b9a25f4f7f1feb9844
Michael Greenly <mgreenly@gmail.com>
2016-05-21 13:48:34 -0500

    create retired folder

---
+++ retired/hello-parsec/app/Main.hs
@@ -0,0 +1,63 @@
+module Main where
+
+import Text.ParserCombinators.Parsec
+
+-- {- A CSV file contains 0 or more lines, each of which is terminated by the end-of-line character (eol). -}
+-- csvFile :: GenParser Char st [[String]]
+-- csvFile = 
+--     do result <- many line
+--        eof
+--        return result
+-- 
+-- -- Each line contains 1 or more cells, separated by a comma
+-- line :: GenParser Char st [String]
+-- line = 
+--     do result <- cells
+--        eol                       -- end of line
+--        return result
+--        
+-- -- Build up a list of cells.  Try to parse the first cell, then figure out what ends the cell.
+-- cells :: GenParser Char st [String]
+-- cells = 
+--     do first <- cellContent
+--        next <- remainingCells
+--        return (first : next)
+-- 
+-- -- The cell either ends with a comma, indicating that 1 or more cells follow,
+-- -- or it doesn't, indicating that we're at the end of the cells for this line
+-- remainingCells :: GenParser Char st [String]
+-- remainingCells =
+--     -- Found comma?  More cells coming No comma?  Return [], no more cells
+--     (char ',' >> cells) <|> (return [])
+-- 
+-- -- Each cell contains 0 or more characters, which must not be a comma or EOL
+-- cellContent :: GenParser Char st String
+-- cellContent = 
+--     many (noneOf ",\n")
+--        
+-- -- The end of line character is \n
+-- eol :: GenParser Char st Char
+-- eol = char '\n'
+-- 
+-- parseCSV :: String -> Either ParseError [[String]]
+-- parseCSV input = parse csvFile "(unknown)" input
+
+csvFile = endBy line eol
+
+line = sepBy cell (char ',')
+
+cell = many (noneOf ",\n")
+
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> fail "couldn't find EOL"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
+main = print $ parseCSV "1 , 2 , 3\n1 , 2, 3\n"
+
+
+
